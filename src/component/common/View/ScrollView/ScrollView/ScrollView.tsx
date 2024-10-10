20b88accec60d5b8c275026fcf745410584ff773
parkdigy <parkdigy@gmail.com>
2024-10-10 16:53:16 +0900

    save

--- src/component/common/View/ScrollView/ScrollView/ScrollView.tsx
+++ src/component/common/View/ScrollView/ScrollView/ScrollView.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import {ScrollView as NativeScrollView, useWindowDimensions} from 'react-native';
+import {useWindowDimensions} from 'react-native';
+import {ScrollView as NativeScrollView} from 'react-native-gesture-handler';
 import CustomComponent from '../../../CustomComponent';
 import {ScrollViewProps as Props} from './ScrollView.types';
 
