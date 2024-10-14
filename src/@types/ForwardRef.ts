e034b29d138fc315ea3318c01f79db83e3cf42c8
parkdigy <parkdigy@gmail.com>
2024-10-14 21:51:07 +0900

    save

--- src/@types/ForwardRef.ts
+++ src/@types/ForwardRef.ts
@@ -10,8 +10,8 @@ export function ToForwardRefExoticComponent<T>(
     Pick<React.ForwardRefExoticComponent<any>, 'displayName' | 'propTypes' | '$$typeof'>;
 }
 
-export function AutoTypeForwardRef<T, P = object>(
-  render: (props: P, ref: React.ForwardedRef<T>) => React.ReactElement | null,
+export function AutoTypeForwardRef<T, P = {}>(
+  render: (props: React.PropsWithoutRef<P>, ref: React.ForwardedRef<T>) => React.ReactElement | null,
 ): (props: P & React.RefAttributes<T>) => React.ReactElement | null {
   return React.forwardRef(render) as (props: P & React.RefAttributes<T>) => React.ReactElement | null;
 }
